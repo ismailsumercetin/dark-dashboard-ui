c1bce9b7a064724bfd2de2a55eb60f365ac5b0d3
sumercetin <sumer@jotform.com>
2022-09-05 00:10:24 +0300

    DarkDashboard :: implement brand image

---
+++ src/components/MenuSection/BrandImage.js
@@ -0,0 +1,7 @@
+export default function BrandImage() {
+    return (
+        <div className="brand-image">
+            <button class="get-pro-button">Get Pro Now</button>
+        </div>
+    );
+};
