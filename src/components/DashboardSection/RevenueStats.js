1a6aa66fb9ecbf09413ee937b54c4d628a5bce7d
sumercetin <sumer@jotform.com>
2022-09-11 13:42:21 +0300

    DarkDashboard :: implement bottom infoboxes

--- src/components/DashboardSection/RevenueStats.js
+++ src/components/DashboardSection/RevenueStats.js
@@ -1,5 +1,10 @@
 export default function RevenueStats() {
     return (
-        <div className="infobox-item revenue-stats"></div>
+        <div className="infobox-item revenue-stats">
+            <div class="infobox-info-container flex flex-col">
+                <h2>Revenue</h2>
+                <span class="infobox-val">$73K</span>
+            </div>
+        </div>
     );
 };
